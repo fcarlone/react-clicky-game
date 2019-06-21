8a6e5176646f4d731854b703457cf9ab686de430
fcarlone <frankcarlone@gmail.com>
2019-06-21 16:01:52 -0400

    Add Score component to display final score

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CharacterItem from "./CharacterItem";
+import Score from "./Score";
 import characters from "../data/characters.json";
 
 class Characters extends React.Component {
@@ -7,7 +8,8 @@ class Characters extends React.Component {
     characters: characters,
     randomCharacters: [],
     selected: [],
-    score: 0
+    score: 0,
+    match: false
   };
 
   // Randomly displayed characters
@@ -22,15 +24,11 @@ class Characters extends React.Component {
     console.log("handle click event", id);
     // Spread operator to track IDs selected
     this.setState({ selected: [...this.state.selected, id] });
-    // Increment score
-    this.setState({ score: this.state.score + 1 });
-    console.log("Characters selected", this.state.selected);
-    console.log("Score", this.state.score);
     // Invoke function to check ID argument against selected array
     this.handleCheckSelection(id);
     // Invoke function to change the random order of characters
     this.handleResortCharacters();
-    // If score equal array.length - game over
+    // If score equal (array.length-1) - game over
   };
 
   handleResortCharacters = () => {
@@ -47,15 +45,28 @@ class Characters extends React.Component {
       this.state.selected
     );
     if (this.state.selected.includes(id)) {
-      console.log("match - DO SOMETHING");
+      console.log("match - end game");
+      this.setState({ match: true });
     } else {
+      // No match - continue game
       console.log("continue");
+      // Increment score
+      this.setState({ score: this.state.score + 1 });
+      console.log("Characters selected", this.state.selected);
+      console.log("Score", this.state.score);
     }
   };
 
   render() {
+    let renderScore = this.state.match;
+    let score;
+
+    if (renderScore) {
+      score = <Score score={this.state.score} />;
+    }
     return (
       <div>
+        {score}
         {console.log(this.state.characters)}
         {this.state.randomCharacters.map(character => (
           <CharacterItem
---
+++ src/components/Score.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
+
+class Score extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Score Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Score;

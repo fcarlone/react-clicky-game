01a72da4a25458cb5d9981ee4590869fdb375c2e
fcarlone <frankcarlone@gmail.com>
2019-06-21 23:39:11 -0400

    Add characterItem css file

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "../styles/characterItem.css";
 
 function CharacterItem(props) {
   // Destructure props
   const { image, id } = props.character;
 
   return (
-    <div className="card" style={{ width: "200px", margin: "1rem" }}>
+    <div className="card" style={{ width: "150px", margin: "1rem" }}>
       <div className="img-container">
         <img
           src={image}
           alt="character"
-          style={{ width: "200px" }}
+          style={{ width: "150px" }}
           onClick={() => props.handleClickEvent(id)}
         />
       </div>

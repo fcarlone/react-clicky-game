39ae91d073cc40bd0582177997da33b2de443cb1
fcarlone <frankcarlone@gmail.com>
2019-06-22 07:35:43 -0400

    Add margin to image tags

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -6,14 +6,13 @@ function CharacterItem(props) {
   const { image, id, name } = props.character;
 
   return (
-    <div className="card" style={{ width: "200px", margin: "1rem" }}>
-      <div className="img-container">
-        <img
-          src={image}
-          alt={name}
-          onClick={() => props.handleClickEvent(id)}
-        />
-      </div>
+    <div className="img-container">
+      <img
+        className="card"
+        src={image}
+        alt={name}
+        onClick={() => props.handleClickEvent(id)}
+      />
     </div>
   );
 }

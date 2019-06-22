9e69f4615db75a14bfe4d1cb23f2f924f633a07c
fcarlone <frankcarlone@gmail.com>
2019-06-21 23:35:22 -0400

    Add characters

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -5,15 +5,12 @@ function CharacterItem(props) {
   const { image, id } = props.character;
 
   return (
-    <div
-      className="card"
-      style={{ width: "100px", backgroundColor: "yellow", margin: "1rem" }}
-    >
+    <div className="card" style={{ width: "200px", margin: "1rem" }}>
       <div className="img-container">
         <img
           src={image}
           alt="character"
-          style={{ width: "100px" }}
+          style={{ width: "200px" }}
           onClick={() => props.handleClickEvent(id)}
         />
       </div>
